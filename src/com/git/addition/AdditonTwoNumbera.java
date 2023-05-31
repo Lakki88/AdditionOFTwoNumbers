a35d556314455a0eaf65cc6b2f00a8247a0defc7
Lakki Turkar <luckyturkar89@gmail.com>
2023-05-31 15:11:26 +0530

    first commit

---
+++ src/com/git/addition/AdditonTwoNumbera.java
@@ -0,0 +1,14 @@
+package com.git.addition;
+
+public class AdditonTwoNumbera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=10;
+		int n2=20;
+		int sum=n1+n2;
+		System.out.println(sum);
+
+	}
+
+}																												
